58dfa3adf8466347af02ad333dfcd2e481b17a44
Filip Gavrilovski <filipgavrilovski28@gmail.com>
2024-05-10 20:28:38 +0200

    added function that adds books to the html container

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const myLibrary = [];
+let myLibrary = [];
+const bookContainer = document.querySelector('.books-container');
 
 function Book(title, author, numPages, haveRead) {
     this.title = title;
@@ -6,26 +7,55 @@ function Book(title, author, numPages, haveRead) {
     this.numPages = numPages;
     this.haveRead = haveRead;
 
-    this.info = () => {
-        let string;
-        string = (this.haveRead === false) ? "not read yet" : "have read";
-        return (this.title + " by " + this.author + ", " + this.numPages 
-            + " pages, " + string);
-    }
+    // this.info = () => {
+    //     let string;
+    //     string = (this.haveRead === false) ? "not read yet" : "have read";
+    //     return (this.title + " by " + this.author + ", " + this.numPages 
+    //         + " pages, " + string);
+    // }
 }
 
 function addBookToLibrary(book){
     myLibrary.push(book);
+    addBookToContainer(book);
 }
 
 const b1 = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 257, false);
 const b2 = new Book("Steve Jobs", "Walter Isaacson", 656, true);
 const b3 = new Book("The Art of War", "Sun Tzu", 68, false);
 
-addBookToLibrary(b1);
-addBookToLibrary(b2);
-addBookToLibrary(b3);
+myLibrary = [b1, b2, b3];
+
 
 myLibrary.forEach((element) => {
     console.table(element.info());
 }) 
+
+
+function addBookToContainer (book) {
+
+let div = document.createElement('div');
+
+const title = document.createElement('h2');
+title.textContent = book.title;
+
+let author = document.createElement('h3');
+author.textContent = book.author;
+
+let numPages = document.createElement('p');
+numPages.textContent = book.numPages;
+
+let haveRead = document.createElement('p');
+haveRead.textContent = book.haveRead;
+
+div.appendChild(title);
+div.appendChild(author);
+div.appendChild(numPages);
+div.appendChild(haveRead);
+
+bookContainer.appendChild(div);
+}
+
+myLibrary.forEach((element) => {
+    addBookToContainer(element);
+})
